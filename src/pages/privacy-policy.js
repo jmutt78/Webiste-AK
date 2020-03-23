3e39cc566ea3d3b38bd4d7abd6effb3626c96a57
jmutt78 <jmcintosh@docusites.com>
2020-03-23 11:38:55 -0700

    added privact policy

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Pat from "../components/NewPat/Pat";
+
+export const Root = styled.div`
+  font-family: "sofia-pro", Sans-serif;
+  padding: 25px 25px 25px 25px;
+  margin: 0 auto 0 auto;
+  max-width: 1280px;
+  hr {
+    text-align: center;
+    margin: 0 auto 3rem auto;
+    border-top: 1.5px solid #8062c6;
+    width: 10%;
+  }
+
+  h2 {
+    text-align: center;
+    font-family: "brandon-grotesque", Sans-serif;
+    font-size: 56px;
+    font-weight: 300;
+    line-height: 1.3em;
+    letter-spacing: 1.7px;
+  }
+  @media (max-width: 600px) {
+    h2 {
+      font-size: 45px;
+    }
+  }
+`;
+
+const Privacy = () => (
+  <Layout>
+    <SEO title="Privacy Policy" />
+    <Root className="my-5">
+      <h2>Privacy Policy</h2>
+      <hr />
+    </Root>
+  </Layout>
+);
+
+export default Privacy;
