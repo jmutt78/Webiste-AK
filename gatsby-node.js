657bdd3b2612a5fe290907b7944364d548cb3722
jmutt78 <jmcintosh@docusites.com>
2020-03-24 13:08:05 -0700

    added privacy policy

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,9 @@
 
 // You can delete this file if you're not using it
 const createPosts = require(`./gatsby/createPosts.js`);
+const createPages = require(`./gatsby/createPages`);
 
 exports.createPages = async ({ actions, graphql }) => {
+  await createPages({ actions, graphql });
   await createPosts({ actions, graphql });
 };
