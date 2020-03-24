1f250475be5aae1e6cad32f0175b4554c89698c3
jmutt78 <jmcintosh@docusites.com>
2020-03-24 08:28:55 -0700

    Book Now page

--- src/pages/book.js
+++ src/pages/book.js
@@ -38,6 +38,16 @@ const Book = () => (
     <Root className="my-5">
       <h2>Schedule an Appointment</h2>
       <hr />
+      <iframe
+        src="https://app.acuityscheduling.com/schedule.php?owner=16433921"
+        width="100%"
+        height="800"
+        frameBorder="0"
+      ></iframe>
+      <script
+        src="https://embed.acuityscheduling.com/js/embed.js"
+        type="text/javascript"
+      ></script>
     </Root>
   </Layout>
 );
